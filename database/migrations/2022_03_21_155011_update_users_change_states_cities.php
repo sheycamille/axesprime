791893422a82514f9701a9786081b610c97d269c
wadleo <wadingaleonard@gmail.com>
2022-03-22 13:11:13 +0100

    Proper way to rename columns

--- database/migrations/2022_03_21_155011_update_users_change_states_cities.php
+++ database/migrations/2022_03_21_155011_update_users_change_states_cities.php
@@ -14,10 +14,8 @@ class UpdateUsersChangeStatesCities extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('town_id');
-            $table->dropColumn('state_id');
-            $table->text('town')->after('address')->nullable();
-            $table->text('state')->after('town')->nullable();
+            $table->renameColumn('town_id', 'town');
+            $table->renameColumn('state_id', 'state');
         });
     }
 
@@ -29,10 +27,8 @@ class UpdateUsersChangeStatesCities extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('town');
-            $table->dropColumn('state');
-            $table->text('town_id')->after('address')->nullable();
-            $table->text('state_id')->after('town_id')->nullable();
+            $table->renameColumn('town', 'town_id');
+            $table->renameColumn('state', 'state_id');
         });
     }
 }
